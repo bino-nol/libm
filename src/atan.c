58adf277fffed88ec9d52fd61ec824afa7213a61
Bino Nol <nika.kru@inbox.ru>
2021-02-04 19:30:34 +0700

    ee

--- src/atan.c
+++ src/atan.c
@@ -1,18 +1,20 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<math.h>
-double atan(double x)
+#include<float.h>
+double a(double x)
 {	
-    double min_e=0x0010000000000000;
     double e=1;
     double sign=-1;
     double _x=x;
     double res=x;
-    double n=1;
-    while(e>=min_e){
-	 n=n+2;
-        _x=_x*x*x*sign;
-	 e=_x/n;
-	 res=res+e;
+    int n=1;
+    if(x<1){
+    	while(fabs(e)>0.000000000000000000000001){
+	   n=n+2;
+           _x=_x*x*x*sign;
+	   e=_x/n;
+	   res=res+e;
+         }
     }
     return res;
 
@@ -22,8 +24,7 @@ double atan(double x)
 
 int main()
 {
-    printf("%e \n",atan(1));
-    printf("%e \n",M_PI_4);
-    printf("%e \n",M_PI_4-atan(1));
+    printf("%.40lf \n",a(0.003));
+    printf("%.40lf \n",atan(0.003));
     return 0;
 }
